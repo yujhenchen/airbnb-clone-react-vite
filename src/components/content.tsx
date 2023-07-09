53df4546e60d19c40ca756a5aca812a5eb9edf9c
yujhenchen <yujhen.chen93@gmail.com>
2023-07-10 00:06:30 +0800

    wider space y between cards

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -7,14 +7,14 @@ export default function Content() {
 
   return (
     <section className="flex flex-col items-center px-10 pt-4 w-full">
-      {/* <div className="px-4 border-box flex place-content-between items-center md:w-1/2 2xl:w-1/3 h-16 w-screen rounded-xl border-2 border-solid border-gray-300 bg-white">
+      <div className="px-4 border-box flex place-content-between items-center md:w-1/2 2xl:w-1/3 h-16 w-screen rounded-xl border-2 border-solid border-gray-300 bg-white">
         <div className="flex space-x-2 ">
           <span className="text-center font-semibold">Display total price</span>
           <span className="text-center">Includes all fees, before taxes</span>
         </div>
         <ToggleSwitch />
-      </div> */}
-      <div className="pt-8 grid pb-16 grid-cols-1 py-2 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6">
+      </div>
+      <div className="pt-8 grid pb-16 grid-cols-1 py-2 gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6">
         {cardIndexLs.map((index) => {
           return <Card key={index} />;
         })}
